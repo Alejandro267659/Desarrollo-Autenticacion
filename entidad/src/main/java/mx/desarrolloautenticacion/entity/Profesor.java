8fcf31c4b81dc76ecedae6795061811814cedd3c
Kalel Baltierra <master.creeperfavorite@gmail.com>
2026-03-08 22:03:49 -0700

    Se modifico el gestionHelper para hacer la union directa de XHTML a la BD

--- entidad/src/main/java/mx/desarrolloautenticacion/entity/Profesor.java
+++ entidad/src/main/java/mx/desarrolloautenticacion/entity/Profesor.java
@@ -37,6 +37,7 @@ public class Profesor {
     private String rfcProfesor;
 
 
+
     public String getNombreProfesor() {
         return nombreProfesor;
     }
--- vista/src/main/java/helper/GestionHelper.java
+++ vista/src/main/java/helper/GestionHelper.java
@@ -1,19 +1,61 @@
 package helper;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import mx.desarrolloautenticacion.dao.ProfesorDAO;
+import mx.desarrolloautenticacion.delegate.DelegateProfesor;
+import mx.desarrolloautenticacion.entity.Profesor;
+import mx.desarrolloautenticacion.facade.FacadeProfesor;
 
+import java.io.Serializable;
+
+@Named("gestionBean")
+@RequestScoped
 public class GestionHelper implements Serializable {
 
-    public boolean guardarMateria(String nombre, String clave, int hClase, int hTaller, int hLab) {
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String rfc;
 
-        System.out.println("Insertando materia: " + nombre);
+    public GestionHelper() {
+        // Constructor vacío, sin crear otra instancia de sí mismo
+    }
+
+    public boolean registrarProfesor() {
+        Profesor p = new Profesor();
+        p.setNombreProfesor(this.nombre);
+        p.setApellidopaternoProfesor(this.apellidoP);
+        p.setApellidoMaternoProfesor(this.apellidoM);
+        p.setRfcProfesor(this.rfc);
+
+        // Aquí deberías llamar a tu capa de negocio/persistencia para guardar el profesor
+        // Ejemplo: profesorService.guardar(p);
+        FacadeProfesor fp = new FacadeProfesor();
+        fp.guardarProfesor(p);
+
+        System.out.println("¡Profesor guardado con éxito!");
+
+        // Limpiar campos después de guardar
+        this.nombre = "";
+        this.apellidoP = "";
+        this.apellidoM = "";
+        this.rfc = "";
+
         return true;
     }
 
-    public boolean vincularProfesorMateria(int idProfesor, int idMateria) {
-        System.out.println("Vinculando Prof: " + idProfesor + " con Materia: " + idMateria);
-        return true;
-    }
+    // --- Getters y Setters ---
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellidoP() { return apellidoP; }
+    public void setApellidoP(String apellidoP) { this.apellidoP = apellidoP; }
+
+    public String getApellidoM() { return apellidoM; }
+    public void setApellidoM(String apellidoM) { this.apellidoM = apellidoM; }
+
+    public String getRfc() { return rfc; }
+    public void setRfc(String rfc) { this.rfc = rfc; }
 }
